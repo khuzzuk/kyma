3787eb5ce5d979f7f04f39def4b61c60d8d45570
khuzzuk <adriandrabik@gmail.com>
2016-10-20 15:58:30 +0200

    commit

--- src/main/java/net/kyma/player/Player.java
+++ src/main/java/net/kyma/player/Player.java
@@ -1,19 +1,20 @@
 package net.kyma.player;
 
-import javafx.application.Application;
+import com.xuggle.mediatool.IMediaReader;
+import com.xuggle.mediatool.IMediaViewer;
+import com.xuggle.mediatool.ToolFactory;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.stage.Stage;
 import lombok.NoArgsConstructor;
 
 import java.io.File;
 
 @NoArgsConstructor
-public class Player extends Application {
+public class Player {
     void play(String path) {
         System.out.println(path.toString());
-        Media media = new Media(new File("D:/Java/kyma/Preludium.mp3").toURI().toString());
+        Media media = new Media(new File("C:/Users/adria/IdeaProjects/kyma/Preludium.mp3").toURI().toString());
         MediaPlayer player = new MediaPlayer(media);
         player.setAutoPlay(true);
         MediaView view = new MediaView(player);
@@ -21,11 +22,8 @@ public class Player extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        play("");
+        IMediaReader reader = ToolFactory.makeReader("Preludium.mp3");
+        reader.addListener(ToolFactory.makeViewer(IMediaViewer.Mode.AUDIO_ONLY));
+        reader.readPacket();
     }
 }
