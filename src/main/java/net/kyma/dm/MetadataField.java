e56a8fbfae72e0b235bf5d6d555691d94c8f3eb5
Adrian Drabik <adriandrabik@gmail.com>
2017-04-30 20:14:21 +0200

    more metadata

--- src/main/java/net/kyma/dm/MetadataField.java
+++ src/main/java/net/kyma/dm/MetadataField.java
@@ -1,10 +1,10 @@
 package net.kyma.dm;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.Year;
-
 @Getter
+@AllArgsConstructor
 public enum MetadataField {
     TITLE("title"),
     PATH("path"),
@@ -12,11 +12,35 @@ public enum MetadataField {
     FILE_NAME("fileName"),
     RATE("rate"),
     YEAR("year"),
-    ALBUM("album");
+    ALBUM("album"),
+    ALBUM_ARTIST("albumArtist"),
+    ALBUM_ARTISTS("albumArtists"),
+    ARTIST("artist"),
+    ARTISTS("artists"),
+    COMPOSER("composer"),
+    CONDUCTOR("conductor"),
+    COUNTRY("country"),
+    CUSTOM1("custom1"),
+    CUSTOM2("custom2"),
+    CUSTOM3("custom3"),
+    CUSTOM4("custom4"),
+    CUSTOM5("custom5"),
+    DISC_NO("discNo"),
+    GENRE("genre"),
+    GROUP("group"),
+    INSTRUMENT("instrument"),
+    MOOD("mood"),
+    MOVEMENT("movement"),
+    OCCASION("occasion"),
+    OPUS("opus"),
+    ORCHESTRA("orchestra"),
+    QUALITY("quality"),
+    RANKING("ranking"),
+    TEMPO("tempo"),
+    TONALITY("tonality"),
+    TRACK("track"),
+    WORK("work"),
+    WORK_TYPE("workType");
 
     private final String name;
-
-    MetadataField(String name) {
-        this.name = name;
-    }
 }
