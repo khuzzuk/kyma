a5379e310d215c00d4ba10b43ece155f7fb0cbc7
Mieszko Cichon <svider.mc@gmail.com>
2017-05-05 19:38:36 +0200

    Change constructor loombok AllArgsConstructor

--- src/main/java/net/kyma/properties/PropertiesManager.java
+++ src/main/java/net/kyma/properties/PropertiesManager.java
@@ -1,12 +1,14 @@
 package net.kyma.properties;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import pl.khuzzuk.messaging.Bus;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -36,7 +38,7 @@ public class PropertiesManager {
     public void initializationProperties() {
         properties = new Properties();
 
-        try (OutputStream output = new FileOutputStream("userProperties.properties")){
+        try (OutputStream output = new FileOutputStream("userProperties.properties")) {
             properties.store(output, "User properties");
         } catch (IOException io) {
             log.error(io);
@@ -45,33 +47,27 @@ public class PropertiesManager {
         bus.setReaction(messages.getProperty("gui.window.settings"), this::windowSettings);
     }
 
-    public void windowSettings() {
+    private void windowSettings() {
+        String maximizedFromProperties = messages.getProperty("player.window.maximized");
+
+        if (BooleanUtils.toBoolean(maximizedFromProperties)) {
+            bus.send(messages.getProperty("gui.window.set.maximized"));
+            return;
+        }
+
         String xFromProperties = messages.getProperty("player.window.x");
         String yFromProperties = messages.getProperty("player.window.y");
-
         String widthFromProperties = messages.getProperty("player.window.width");
         String heightFromProperties = messages.getProperty("player.window.height");
 
-        String maximizedFromProperties = messages.getProperty("player.window.maximized");
 
-        if(NumberUtils.isDigits(xFromProperties) && NumberUtils.isDigits(yFromProperties)
-                && NumberUtils.isDigits(widthFromProperties) && NumberUtils.isDigits(heightFromProperties))
-            if (NumberUtils.isParsable(xFromProperties) && NumberUtils.isParsable(yFromProperties)
-                    && NumberUtils.isParsable(widthFromProperties) && NumberUtils.isParsable(heightFromProperties)){
-
-                int x = NumberUtils.toInt(xFromProperties);
-                int y = NumberUtils.toInt(yFromProperties);
-
-                bus.send(properties.getProperty("player.window.x"), x);
-                bus.send(properties.getProperty("player.window.y"), y);
-
-                int width = NumberUtils.toInt(widthFromProperties);
-                int height = NumberUtils.toInt(heightFromProperties);
-
-                bus.send(properties.getProperty("player.window.width"), width);
-                bus.send(properties.getProperty("player.window.height"), height);
-
-                bus.send(properties.getProperty("player.window.maximized"), maximizedFromProperties);
-            }
+        if (NumberUtils.isDigits(xFromProperties) && NumberUtils.isDigits(yFromProperties)
+                && NumberUtils.isDigits(widthFromProperties) && NumberUtils.isDigits(heightFromProperties)) {
+            int y = NumberUtils.toInt(yFromProperties);
+            int width = NumberUtils.toInt(widthFromProperties);
+            int height = NumberUtils.toInt(heightFromProperties);
+            Rectangle rectangle = new Rectangle(NumberUtils.toInt(xFromProperties), y, width, height);
+            bus.send(messages.getProperty("gui.window.set.frame"), rectangle);
+        }
     }
 }
