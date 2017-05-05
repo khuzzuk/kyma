6c6444aec45a813965853be3d652daa642aaf541
Mieszko Cichon <svider.mc@gmail.com>
2017-05-05 19:39:30 +0200

    Change constructor loombok AllArgsConstructor

--- src/main/java/net/kyma/properties/PropertiesField.java
+++ src/main/java/net/kyma/properties/PropertiesField.java
@@ -1,14 +1,12 @@
 package net.kyma.properties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum PropertiesField {
     MUSICPOSITION("musicPosition");
 
     private final String name;
-
-    PropertiesField(String name) {
-        this.name = name;
-    }
 }
