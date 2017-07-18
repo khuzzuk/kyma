3b5488ca992990851f49e8b85656ddbc2a34cfca
Mieszko Cichon <svider.mc@gmail.com>
2017-07-18 09:40:34 +0200

    Serialize/deserialize user properties - refactoring

--- src/main/java/net/kyma/properties/ColumnManager.java
+++ src/main/java/net/kyma/properties/ColumnManager.java
@@ -5,58 +5,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import net.kyma.dm.MetadataField;
 import pl.khuzzuk.messaging.Bus;
+
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 @Log4j2
 public class ColumnManager {
     @Inject
     private Bus bus;
+
     @Inject
     @Named("messages")
     private Properties messages;
+
     @Inject
     private ObjectMapper objectMapper;
-    private LinkedHashMap<MetadataField, Double> linkedHashMap;
+
     private static File file;
 
-    public void init()
-    {
-        objectMapper = new ObjectMapper();
-        file = new File("columnsProperties.json");
-        if (!file.exists()) {
-            log.fatal("File is not exists.");
-            System.exit(-1);
-        }
+    public void init() {
+        file = new File(getClass().getResource("/columnsProperties.json").getFile());
 
         bus.setReaction(messages.getProperty("properties.columns.settings.store"), this::serializeJson);
-        bus.setReaction(messages.getProperty("properties.collumns.settings.get"), this::deserializeJson);
+        bus.setReaction(messages.getProperty("properties.columns.settings.get"),
+                () -> Optional.ofNullable(deserializeJson())
+                        .ifPresent(columns -> bus.send(messages.getProperty("gui.columns.set"), columns)));
     }
 
-    private synchronized void serializeJson()
-    {
-        TypeReference<LinkedHashMap<MetadataField, Double>> typeReference =
-                new TypeReference<LinkedHashMap<MetadataField, Double>>() {};
+    private synchronized void serializeJson(Map<MetadataField, Double> columns) {
         try {
-            linkedHashMap = objectMapper.readValue(file, typeReference);
+            objectMapper.writeValue(file, columns);
         } catch (IOException e) {
-            log.fatal("Can not serialize user properties from file.");
-            log.fatal(e);
+            log.error("Can not deserialize user properties to file.");
+            log.error(e);
             e.printStackTrace();
         }
     }
 
-    private synchronized void deserializeJson()
-    {
+    private synchronized LinkedHashMap<MetadataField, Double> deserializeJson() {
         try {
-            objectMapper.writeValue(file, linkedHashMap);
+            return objectMapper.readValue(file, new TypeReference<LinkedHashMap<MetadataField, Double>>() {});
         } catch (IOException e) {
-            log.fatal("Can not deserialize user properties to file.");
-            log.fatal(e);
+            log.error("Can not serialize user properties from file.");
+            log.error(e);
             e.printStackTrace();
         }
+        return null;
     }
 }
