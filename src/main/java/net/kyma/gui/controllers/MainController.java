f5d38fa811a34989c8429a04f812d6d5b97ad228
Adrian Drabik <adrian.drabik@silvermedia.pl>
2018-04-09 15:00:10 +0200

    remove jaad until fixed bugs catching mp3 files by mp4 spi

--- src/main/java/net/kyma/gui/controllers/MainController.java
+++ src/main/java/net/kyma/gui/controllers/MainController.java
@@ -12,7 +12,7 @@ import static net.kyma.EventType.GUI_WINDOW_SET_MAXIMIZED;
 import static net.kyma.EventType.PLAYLIST_ADD_FILE;
 import static net.kyma.EventType.PLAYLIST_ADD_SOUND;
 
-import java.awt.*;
+import java.awt.Rectangle;
 import java.io.File;
 import java.net.URL;
 import java.util.Optional;
@@ -20,19 +20,11 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.SplitPane;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -66,14 +58,8 @@ public class MainController implements Initializable {
               .<Number>accept(n -> indicator.setProgress(n.doubleValue() / maxProgress)).subscribe();
         bus.subscribingFor(DATA_INDEXING_FINISH).onFXThread()
               .then(() -> mainPane.getItems().remove(indicator)).subscribe();
-        bus.subscribingFor(GUI_WINDOW_SET_FULLSCREEN).onFXThread().then(() -> {
-            stage.setFullScreen(true);
-            setBackground();
-        }).subscribe();
-        bus.subscribingFor(GUI_WINDOW_SET_MAXIMIZED).onFXThread().then(() -> {
-            stage.setMaximized(true);
-            setBackground();
-            }).subscribe();
+        bus.subscribingFor(GUI_WINDOW_SET_FULLSCREEN).onFXThread().then(() -> stage.setFullScreen(true)).subscribe();
+        bus.subscribingFor(GUI_WINDOW_SET_MAXIMIZED).onFXThread().then(() -> stage.setMaximized(true)).subscribe();
         bus.subscribingFor(GUI_WINDOW_SET_FRAME).accept(this::resize).subscribe();
 
         bus.message(GUI_WINDOW_SETTINGS).send();
@@ -86,14 +72,6 @@ public class MainController implements Initializable {
         indicator = new ProgressIndicator();
         mainPane.getItems().add(indicator);
         indicator.setVisible(true);
-    }
-
-    private void setBackground() {
-        Rectangle2D screenSize = Screen.getPrimary().getBounds();
-        mainPane.setBackground(new Background(new BackgroundImage(new Image("/css/classic-background.png"),
-                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
-                new BackgroundSize(screenSize.getWidth(), screenSize.getHeight(),
-                        false, false, false, false))));
     }
 
     @FXML
@@ -126,6 +104,5 @@ public class MainController implements Initializable {
         stage.setY(r.getY());
         stage.setWidth(r.getWidth());
         stage.setHeight(r.getHeight());
-        setBackground();
     }
 }
